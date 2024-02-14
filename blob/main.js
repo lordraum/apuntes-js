490860a0a502b97a9b417ab90b7d32828b1bcc52
Fernando Gómez <davidf.gomez@hotmail.com>
2024-02-14 10:24:21 -0500

    blob

---
+++ blob/main.js
@@ -0,0 +1,26 @@
+// Blob Representa a los archivos como data (raw)
+//URL.createObjectURL(blob) crea una url para acceder al recurso
+
+const catButton = document.getElementById('cat-button')
+const dogButton = document.getElementById('dog-button')
+const image = document.getElementById('image')
+
+const catUrl = image.attributes.src.value = '../img/cat.webp'
+const dogUrl = image.attributes.src.value = '../img/dog.webp'
+
+
+catButton.addEventListener('click', (e) => {
+  fetch(catUrl)
+    .then(x => x.blob())
+    .then(x => {
+      image.src = URL.createObjectURL(x)
+    })
+})
+
+dogButton.addEventListener('click', (e) => {
+  fetch(dogUrl)
+    .then(x => x.blob())
+    .then(x => {
+      image.src = URL.createObjectURL(x)
+    })
+})
